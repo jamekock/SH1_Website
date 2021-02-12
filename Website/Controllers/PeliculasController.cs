83a665a43bccb3fbc6ffd1bd8945e5ad7c48a64e
jamekock <eliasplaying@gmail.com>
2021-02-12 14:04:07 -0400

    Add project files.

---
+++ Website/Controllers/PeliculasController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        // POST: ../Peliculas/
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            ViewBag.Peliculas = collection;
+            return View();
+        }
+
+        // GET: Peliculas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
